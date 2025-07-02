3d9af30a2a35f9614bed334218c33314f59b028c
Xryzo11 <Xryzo11@o2.pl>
2025-07-02 02:50:49 +0200

    Transform global commands into guild commands

--- src/main/java/com/xryzo11/discordbot/DiscordBot.java
+++ src/main/java/com/xryzo11/discordbot/DiscordBot.java
@@ -1,6 +1,7 @@
 package com.xryzo11.discordbot;
 
 import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
@@ -13,19 +14,21 @@ public class DiscordBot {
                 .addEventListeners(new SlashCommands.SlashCommandInteractionEventListener(musicBot))
                 .build();
         BotHolder.setJDA(jda);
-        jda.updateCommands().addCommands(
-                Commands.slash("join", "Join voice channel"),
-                Commands.slash("queue", "Queue YouTube audio")
-                        .addOption(OptionType.STRING, "url", "YouTube URL", true),
-                Commands.slash("pause", "Pause current playback"),
-                Commands.slash("resume", "Resume playback"),
-                Commands.slash("clear", "Clear the queue"),
-                Commands.slash("stop", "Stop playback and disconnect"),
-                Commands.slash("list", "List current queue"),
-                Commands.slash("skip", "Skip the current track"),
-                Commands.slash("loop", "Toggle track looping")
-        ).queue();
         WywozBindingManager.loadBindings();
         Dashboard.start();
+        for (Guild guild : jda.getGuilds()) {
+            guild.updateCommands().addCommands(
+                    Commands.slash("join", "Join voice channel"),
+                    Commands.slash("queue", "Queue YouTube audio")
+                            .addOption(OptionType.STRING, "url", "YouTube URL", true),
+                    Commands.slash("pause", "Pause current playback"),
+                    Commands.slash("resume", "Resume playback"),
+                    Commands.slash("clear", "Clear the queue"),
+                    Commands.slash("stop", "Stop playback and disconnect"),
+                    Commands.slash("list", "List current queue"),
+                    Commands.slash("skip", "Skip the current track"),
+                    Commands.slash("loop", "Toggle track looping")
+            ).queue();
+        }
     }
 }
