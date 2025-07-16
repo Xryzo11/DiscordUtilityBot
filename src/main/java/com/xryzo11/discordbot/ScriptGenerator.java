95ec62ca77242d47694604055a59020c87708f60
Xryzo11 <Xryzo11@o2.pl>
2025-07-16 02:56:25 +0200

    Fixed indentations in start script

--- src/main/java/com/xryzo11/discordbot/ScriptGenerator.java
+++ src/main/java/com/xryzo11/discordbot/ScriptGenerator.java
@@ -26,8 +26,8 @@ public class ScriptGenerator {
                 fw.write("source /etc/profile\n");
                 fw.write("while (true); do\n");
                 fw.write("  clear\n");
-                fw.write("java --enable-native-access=ALL-UNNAMED -jar \"" + jarName + "\"\n");
-                fw.write("sleep 3\n");
+                fw.write("  java --enable-native-access=ALL-UNNAMED -jar \"" + jarName + "\"\n");
+                fw.write("  sleep 3\n");
                 fw.write("done\n");
             }
             startScript.setExecutable(true);
