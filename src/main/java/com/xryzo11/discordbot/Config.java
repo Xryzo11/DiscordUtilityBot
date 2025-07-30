d7cd7f4c8da06a841f9ce8da11153fc95a750047
Xryzo11 <Xryzo11@o2.pl>
2025-07-30 04:20:19 +0200

    Web panel authentication

--- src/main/java/com/xryzo11/discordbot/Config.java
+++ src/main/java/com/xryzo11/discordbot/Config.java
@@ -1,6 +1,7 @@
 package com.xryzo11.discordbot;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Properties;
 
@@ -67,7 +68,7 @@ public class Config {
     public static String sha512(String input) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-512");
-            byte[] hash = md.digest(input.getBytes());
+            byte[] hash = md.digest(input.getBytes(StandardCharsets.UTF_8));
             StringBuilder sb = new StringBuilder();
             for (byte b : hash) sb.append(String.format("%02x", b));
             return sb.toString();
