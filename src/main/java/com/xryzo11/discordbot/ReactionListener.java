db02c027469522df880ef8e4a522a0af806ff3e5
Xryzo11 <Xryzo11@o2.pl>
2025-10-07 17:33:36 +0200

    New universal confirmation listener

---
+++ src/main/java/com/xryzo11/discordbot/ReactionListener.java
@@ -0,0 +1,31 @@
+package com.xryzo11.discordbot;
+
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class ReactionListener extends ListenerAdapter {
+    public static boolean awaitingConfirmation = false;
+    public static boolean confirmed = false;
+
+    @Override
+    public void onMessageReactionAdd(MessageReactionAddEvent event) {
+        if (event.getUser().isBot()) {
+            confirmed = false;
+            return;
+        }
+
+        if (event.getUser() == null) {
+            return;
+        }
+
+        if (awaitingConfirmation) {
+            if (event.getMessageAuthorId().equals(event.getJDA().getSelfUser().getId())) {
+                if (event.getEmoji().getName().equals("✅")) {
+                    confirmed = true;
+                    awaitingConfirmation = false;
+                    return;
+                }
+            }
+        }
+    }
+}
