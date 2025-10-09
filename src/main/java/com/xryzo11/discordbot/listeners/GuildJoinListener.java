f12a96e61c3b97d80f3c82f0b37fbbdadae95bdb
Xryzo11 <xryzo11@o2.pl>
2025-10-09 20:24:39 +0200

    Renamed listener class

--- src/main/java/com/xryzo11/discordbot/listeners/GuildJoinListener.java
+++ src/main/java/com/xryzo11/discordbot/listeners/GuildJoinListener.java
@@ -1,14 +1,14 @@
 package com.xryzo11.discordbot.listeners;
 
 import com.xryzo11.discordbot.Config;
+import com.xryzo11.discordbot.RoleRestorer;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
-public class JoinListener extends ListenerAdapter {
+public class GuildJoinListener extends ListenerAdapter {
 
     @Override
     public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
@@ -16,5 +16,7 @@ public class JoinListener extends ListenerAdapter {
         Guild guild = event.getGuild();
 
         if (Config.isDebugEnabled()) System.out.println("[JoinListener] New member joined: " + member.getEffectiveName());
+
+        RoleRestorer.restoreRole(event);
     }
 }
