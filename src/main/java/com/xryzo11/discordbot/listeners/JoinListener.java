60797ec523ee49820af1c6a8eedd245aa67e8a2b
Xryzo11 <xryzo11@o2.pl>
2025-10-09 20:18:20 +0200

    Guild join listener

---
+++ src/main/java/com/xryzo11/discordbot/listeners/JoinListener.java
@@ -0,0 +1,20 @@
+package com.xryzo11.discordbot.listeners;
+
+import com.xryzo11.discordbot.Config;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class JoinListener extends ListenerAdapter {
+
+    @Override
+    public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
+        Member member = event.getMember();
+        Guild guild = event.getGuild();
+
+        if (Config.isDebugEnabled()) System.out.println("[JoinListener] New member joined: " + member.getEffectiveName());
+    }
+}
