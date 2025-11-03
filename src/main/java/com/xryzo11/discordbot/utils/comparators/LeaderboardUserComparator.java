50839c2f1ab69c2d7a891207b182e31c4c96a796
Xryzo11 <Xryzo11@o2.pl>
2025-11-03 21:40:35 +0100

    Improved comparator

--- src/main/java/com/xryzo11/discordbot/utils/comparators/LeaderboardUserComparator.java
+++ src/main/java/com/xryzo11/discordbot/utils/comparators/LeaderboardUserComparator.java
@@ -7,6 +7,13 @@ import java.util.Comparator;
 public class LeaderboardUserComparator implements Comparator<LeaderboardUser> {
     @Override
     public int compare(LeaderboardUser user1, LeaderboardUser user2) {
-        return Integer.compare(user2.getMessagesSent(), user1.getMessagesSent());
+        int comparison = Integer.compare(user2.getXp(), user1.getXp());
+        if (comparison == 0) {
+            comparison = Integer.compare(user2.getMessagesSent(), user1.getMessagesSent());
+        }
+        if (comparison == 0) {
+            comparison = user1.getUserId().compareTo(user2.getUserId());
+        }
+        return comparison;
     }
 }
